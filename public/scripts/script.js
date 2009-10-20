a1418680a53a3f1d25b8cb476722f017947469b0
Emily Price <price.emily@gmail.com>
2009-10-20 11:52:17 -0400

    Load sidebar on page load

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,7 @@
+$(function(){
+  $("#sidebar .content").load("/pages?embed=true");
+});
+
 function add_and_bind_form( to, page_id, type ) {
   to.load("/pages/" + page_id + "/contents/new?type=" + type, function() { 
     $(this).find("form").ajaxForm({ success: function(data, textStatus){ alert( data + "..." + textStatus );}});
