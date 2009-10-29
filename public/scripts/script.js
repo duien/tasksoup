0f51144c1a208abf8ea9d1c5a1f84ac16524c941
Emily Price <price.emily@gmail.com>
2009-10-29 11:23:05 -0400

    Updated forms to no longer use 'type'

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,16 +2,15 @@ $(function(){
   $("#sidebar .content").load("/pages?embed=true");
 });
 
-function add_and_bind_form( to, page_id, type ) {
-  to.load("/pages/" + page_id + "/contents/new?embed=true&type=" + type, function() { 
+function add_and_bind_form( to, page_id ) {
+  to.load("/pages/" + page_id + "/tasks/new?embed=true", function() { 
     $(this).find("form").ajaxForm({ success: function(data, textStatus){
-      // alert( data + "..." + textStatus );
       $("#main .content").html( data );
     }});
-    $(this).find("#"+type+"_expand").click(function(){
-      var input = $(this).siblings("#"+type+"_text");
+    $(this).find("#task_expand").click(function(){
+      var input = $(this).siblings("#task_text");
       var value = input.val();
-      input.replaceWith( "<textarea name='"+type+"[text]' id='"+type+"_text'>"+value+"</textarea>" );
+      input.replaceWith( "<textarea name='task[text]' id='task_text'>"+value+"</textarea>" );
       $(this).remove();
     });
   });
