836a2bcb1702a835d284580e8cef40ab8f40741c
Emily Price <price.emily@gmail.com>
2009-11-16 17:59:29 -0500

    Made cancel button on add task for work, updated assoc. styling

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,15 +4,23 @@ $(function(){
 
 function add_and_bind_form( to, project_id ) {
   to.load("/projects/" + project_id + "/tasks/new?embed=true", function() { 
-    $(this).find("form").ajaxForm({ success: function(data, textStatus){
+    $("form").ajaxForm({ success: function(data, textStatus){
       $("#main .content").html( data );
     }});
-    $(this).find("#task_expand").click(function(){
+    $("#task_expand").click(function(){
       var input = $(this).siblings("#task_text");
       var value = input.val();
       input.replaceWith( "<textarea name='task[text]' id='task_text'>"+value+"</textarea>" );
       $(this).remove();
     });
+    $("#task_cancel").click(function(){
+      to.siblings('.add_link').css('display', 'inline');
+      to.html( "" );
+      to.css('display', 'none');
+    });
+    $(this).siblings('.add_link').css('display', 'none');
+    $(this).css('display', 'block');
+    $("#task_text").focus();
   });
 };
 
